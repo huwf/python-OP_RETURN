1d6cfa05d29cf2e9e72e344c6cec14e26ae6cfb8
Huw Fryer <huwfryer@gmail.com>
2017-10-03 13:57:03 +0100

    Added .gitignore and changed tabs to spaces

--- store-OP_RETURN.py
+++ store-OP_RETURN.py
@@ -26,28 +26,28 @@
 import sys, string
 from OP_RETURN import *
 
-
-if len(sys.argv)<2:
-	sys.exit(
-'''Usage:
+if len(sys.argv) < 2:
+    sys.exit(
+        '''Usage:
 python store-OP_RETURN.py <data> <testnet (optional)>'''
-	)
-	
-data=sys.argv[1]
+    )
 
-if len(sys.argv)>2:
-	testnet=bool(sys.argv[2])
+data = sys.argv[1]
+
+if len(sys.argv) > 2:
+    testnet = bool(sys.argv[2])
 else:
-	testnet=False
+    testnet = False
 
-data_from_hex=OP_RETURN_hex_to_bin(data)
+data_from_hex = OP_RETURN_hex_to_bin(data)
 if data_from_hex is not None:
-	data=data_from_hex
+    data = data_from_hex
 
-result=OP_RETURN_store(data, testnet)
+result = OP_RETURN_store(data, testnet)
 
 if 'error' in result:
-	print('Error: '+result['error'])
+    print('Error: ' + result['error'])
 else:
-	print("TxIDs:\n"+"\n".join(result['txids'])+"\n\nRef: "+result['ref']+"\n\nWait a few seconds then check on: http://"+
-		('testnet.' if testnet else '')+'coinsecrets.org/')
+    print("TxIDs:\n" + "\n".join(result['txids']) + "\n\nRef: " + result[
+        'ref'] + "\n\nWait a few seconds then check on: http://" +
+          ('testnet.' if testnet else '') + 'coinsecrets.org/')
